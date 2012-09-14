70216573baef6f1b69cbdf2494390eb7d736593e
philia <philia.fan@gmail.com>
2012-09-14 10:41:01 +0800

    added git scripts to clone all plugins

---
+++ vim.plugins.git.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone git@github.com:tpope/vim-pathogen
+git clone git@github.com:nathanaelkane/vim-indent-guides
+git clone git@github.com:edsono/vim-matchit
+git clone git@github.com:Lokaltog/vim-powerline
