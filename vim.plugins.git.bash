b723fbab725332bc406386ca6f9a00a2a03319e5
Philia <philia.fan@gmail.com>
2012-09-15 17:32:51 +0800

    added install script for pathogen

--- vim.plugins.git.bash
+++ vim.plugins.git.bash
@@ -3,3 +3,7 @@ git clone git@github.com:tpope/vim-pathogen
 git clone git@github.com:nathanaelkane/vim-indent-guides
 git clone git@github.com:edsono/vim-matchit
 git clone git@github.com:Lokaltog/vim-powerline
+
+#install
+#mkdir -p ~/.vim/autoload ~/.vim/bundle;
+#curl -so ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
