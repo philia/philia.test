a7d8fe1b4c8bbb870c2dda1151013138e1130b3c
philia.fan <philia.fan@gmail.com>
2012-12-13 22:02:57 +0800

    updated with more vim plugins

--- vim.plugins.git.bash
+++ vim.plugins.git.bash
@@ -3,6 +3,8 @@ git clone git@github.com:tpope/vim-pathogen
 git clone git@github.com:nathanaelkane/vim-indent-guides
 git clone git@github.com:edsono/vim-matchit
 git clone git@github.com:Lokaltog/vim-powerline
+git clone git@github.com:thisivan/vim-taglist
+git clone git@github.com:tpope/vim-surround
 
 #install
 #copy pathogen.vim to $VIMRUNTIME/autoload/
