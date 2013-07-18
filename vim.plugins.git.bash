db224eaf8fcb66c7bbb55940e3978f7ee4de75fe
hfan.vm126 <hfan@adobe.com>
2013-07-18 04:12:06 -0700

    set HTTPS as default url

--- vim.plugins.git.bash
+++ vim.plugins.git.bash
@@ -1,19 +1,19 @@
 #!/bin/bash
-git clone git@github.com:tpope/vim-pathogen.git
-git clone git@github.com:nathanaelkane/vim-indent-guides.git
-git clone git@github.com:edsono/vim-matchit.git
-git clone git@github.com:Lokaltog/vim-powerline.git
-git clone git@github.com:Lokaltog/vim-easymotion.git
-git clone git@github.com:thisivan/vim-taglist.git
-git clone git@github.com:tpope/vim-surround.git
-git clone git@github.com:kien/ctrlp.vim.git
-#git clone git@github.com:sjbach/lusty.git
-git clone git@github.com:Shougo/neosnippet.git
-git clone git@github.com:Shougo/neocomplcache.git
-git clone git@github.com:scrooloose/nerdtree.git
-git clone git@github.com:godlygeek/tabular.git
+# GIT version
+#git clone git@github.com:tpope/vim-pathogen.git
+#git clone git@github.com:nathanaelkane/vim-indent-guides.git
+#git clone git@github.com:edsono/vim-matchit.git
+#git clone git@github.com:Lokaltog/vim-powerline.git
+#git clone git@github.com:Lokaltog/vim-easymotion.git
+#git clone git@github.com:thisivan/vim-taglist.git
+#git clone git@github.com:tpope/vim-surround.git
+#git clone git@github.com:kien/ctrlp.vim.git
+#git clone git@github.com:Shougo/neosnippet.git
+#git clone git@github.com:Shougo/neocomplcache.git
+#git clone git@github.com:scrooloose/nerdtree.git
+#git clone git@github.com:godlygeek/tabular.git
 
-wget http://s3.wincent.com/command-t/releases/command-t-1.4.vba
+#wget http://s3.wincent.com/command-t/releases/command-t-1.4.vba
 
 #install
 #copy pathogen.vim to $VIMRUNTIME/autoload/
@@ -22,3 +22,18 @@ wget http://s3.wincent.com/command-t/releases/command-t-1.4.vba
 # :e name.vba
 # :!mkdir $PATH_TO_VIMFILES/bundle/name
 # :UseVimball $PATH_TO_VIMFILES/bundle/name
+
+# HTTPS version
+git clone https://github.com/tpope/vim-pathogen.git
+git clone https://github.com/nathanaelkane/vim-indent-guides.git
+git clone https://github.com/edsono/vim-matchit.git
+git clone https://github.com/Lokaltog/vim-powerline.git
+git clone https://github.com/Lokaltog/vim-easymotion.git
+git clone https://github.com/thisivan/vim-taglist.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/Shougo/neosnippet.git
+git clone https://github.com/Shougo/neocomplcache.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/godlygeek/tabular.git
+
