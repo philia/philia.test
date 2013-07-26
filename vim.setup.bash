189bcd58ecf9ab4967ab6b1be3c431ab09e33f12
hfan.vm249 <hfan@adobe.com>
2013-07-26 01:30:20 -0700

    added vim source

--- vim.setup.bash
+++ vim.setup.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
-# Compile vim with python
-./configure --prefix=/home/httpd/work/opt/ --enable-pythoninterp --with-python-config-dir=/home/httpd/work/opt/lib/python2.7/config --with-features=huge
 
-# Plugins Section
-# GIT version
+## Get Latest VIM
+#yum install mercurial
+#hg clone https://vim.googlecode.com/hg/ vim
+
+## Compile vim with python
+#./configure --prefix=/home/httpd/work/opt/ --enable-pythoninterp --with-python-config-dir=/home/httpd/work/opt/lib/python2.7/config --with-features=huge
+
+## Plugins Section
 #git clone git@github.com:tpope/vim-pathogen.git
 #git clone git@github.com:tpope/vim-surround.git
 #git clone git@github.com:nathanaelkane/vim-indent-guides.git
@@ -16,7 +20,7 @@
 #git clone git@github.com:godlygeek/tabular.git
 #git clone git@github.com:Valloric/YouCompleteMe.git
 
-#install
+##install
 #copy pathogen.vim to $VIMRUNTIME/autoload/
 #git clone to $PATH_TO_VIMFILES/bundle/
 #use the followed command to install vimball package:
@@ -24,15 +28,3 @@
 # :!mkdir $PATH_TO_VIMFILES/bundle/name
 # :UseVimball $PATH_TO_VIMFILES/bundle/name
 
-# HTTPS version
-git clone https://github.com/tpope/vim-pathogen.git
-git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/nathanaelkane/vim-indent-guides.git
-git clone https://github.com/edsono/vim-matchit.git
-git clone https://github.com/Lokaltog/vim-powerline.git
-git clone https://github.com/Lokaltog/vim-easymotion.git
-git clone https://github.com/thisivan/vim-taglist.git
-git clone https://github.com/kien/ctrlp.vim.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/godlygeek/tabular.git
-git clone https://github.com/Valloric/YouCompleteMe.git
