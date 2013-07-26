37622633d8f081d56b494f09edfeee431a3a8792
hfan.vm249 <hfan@adobe.com>
2013-07-26 01:22:51 -0700

    Added vimfiles as submodule

--- vim.plugins.git.bash
+++ vim.plugins.git.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
-# Compile vim
+# Compile vim with python
+./configure --prefix=/home/httpd/work/opt/ --enable-pythoninterp --with-python-config-dir=/home/httpd/work/opt/lib/python2.7/config --with-features=huge
 
 # Plugins Section
 # GIT version
 #git clone git@github.com:tpope/vim-pathogen.git
+#git clone git@github.com:tpope/vim-surround.git
 #git clone git@github.com:nathanaelkane/vim-indent-guides.git
 #git clone git@github.com:edsono/vim-matchit.git
 #git clone git@github.com:Lokaltog/vim-powerline.git
 #git clone git@github.com:Lokaltog/vim-easymotion.git
 #git clone git@github.com:thisivan/vim-taglist.git
-#git clone git@github.com:tpope/vim-surround.git
 #git clone git@github.com:kien/ctrlp.vim.git
 #git clone git@github.com:scrooloose/nerdtree.git
 #git clone git@github.com:godlygeek/tabular.git
@@ -25,12 +26,12 @@
 
 # HTTPS version
 git clone https://github.com/tpope/vim-pathogen.git
+git clone https://github.com/tpope/vim-surround.git
 git clone https://github.com/nathanaelkane/vim-indent-guides.git
 git clone https://github.com/edsono/vim-matchit.git
 git clone https://github.com/Lokaltog/vim-powerline.git
 git clone https://github.com/Lokaltog/vim-easymotion.git
 git clone https://github.com/thisivan/vim-taglist.git
-git clone https://github.com/tpope/vim-surround.git
 git clone https://github.com/kien/ctrlp.vim.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/godlygeek/tabular.git
