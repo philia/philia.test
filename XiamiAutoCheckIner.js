c7b53f3117a049f9796b4a86409997f7919b9938
hfan <hfan@adobe.com>
2013-10-23 10:15:29 +0800

    Updated autocheckiner based on new ui

--- XiamiAutoCheckIner.js
+++ XiamiAutoCheckIner.js
@@ -3,7 +3,7 @@
 // @namespace  http://use.i.E.your.homepage/
 // @version    0.1
 // @description  Auto check-in button clicker for Xiami music
-// @match      http://www.xiami.com/
+// @match      http://www.xiami.com/home
 // @copyright  2012+, You
 // @require    https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
 // ==/UserScript==
@@ -15,10 +15,15 @@ GM_log("PD: $().jquery: " + $().jquery);
 // This only fires the function assigned to the click event. It will not navigate to the path specified in the href attribute.
 //$(document).ready($("#check_in").click());
 
-$(document).ready(function() {
-    // Auto Click on check in
-    var elem = document.getElementById("check_in");
-    GM_log("PD: elem = " + elem);
+function autocheckin() {
+    var elem = $('#rank .action .tosign');
+    if (typeof elem.get(0) === "undefined") {
+        GM_log("PD: no action element found, waiting....");
+        setTimeout(autocheckin, 1000);
+        return;
+    }
+    elem = elem.get(0);
+
     if (document.dispatchEvent) {
         // W3C
         var oEvent = document.createEvent("MouseEvents");
@@ -28,4 +33,10 @@ $(document).ready(function() {
         // IE
         elem.click();
     }
+    GM_log("PD: Checked in");
+}
+
+$(document).ready(function() {
+    // Auto Click on check in
+	autocheckin();
 });
