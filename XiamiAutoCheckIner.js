3b29cf083a18fa5c0bae55005cbe4b27d6eedffe
git <git@github.com>
2013-11-26 21:23:55 -0800

    Update due to latest UI changes

--- XiamiAutoCheckIner.js
+++ XiamiAutoCheckIner.js
@@ -16,7 +16,7 @@ GM_log("PD: $().jquery: " + $().jquery);
 //$(document).ready($("#check_in").click());
 
 function autocheckin() {
-    var elem = $('#rank .action .tosign');
+    var elem = $('b.icon.toSign');
     if (typeof elem.get(0) === "undefined") {
         GM_log("PD: no action element found, waiting....");
         setTimeout(autocheckin, 1000);
